c353ffe3c21650a2bc521addb48dcc8109d5371e
lregebro <lregebro@7952cd18-87d2-4e94-9fb8-aee74ce2027a>
2013-05-04 13:45:52 +0000

    Reorg.

--- buildout/products/PropertySets/interfaces/predicate.py
+++
@@ -1,9 +0,0 @@
-from Interface import Interface
-
-class IPropertySetPredicate(Interface):
-
-    def apply(obj):
-        """Return the PropertySets that apply to the given object
-        after satisfying the predicate conditions
-        """
-
