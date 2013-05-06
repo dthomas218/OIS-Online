b829145edfdce6dca80f967357ef3ee0641a133d
lregebro <lregebro@7952cd18-87d2-4e94-9fb8-aee74ce2027a>
2013-05-06 09:51:06 +0000

    Updates for production.

--- products/CMFPropertySets/interfaces/__init__.py
+++
@@ -1,9 +0,0 @@
-# import this
-
-from Products.PropertySets.interfaces.registry import IPropertySetRegistry
-
-class ILocalVocabRegistry(IPropertySetRegistry):
-    """Local Vocabulary Registry"""
-
-class IGlobalVocabRegistry(ILocalVocabRegistry):
-    """Global Vocabulary Registry"""
